fd2a746be9d01efc557ba6aac8f8d246487ef549
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-21 08:11:55 +0800

    implemented map object to previous bio.js

--- bio.js
+++ bio.js
@@ -1,13 +1,35 @@
 const fs = require('fs')
 const csvjson = require('csvjson')
 
+const titleCase = (str) => str.charAt(0).toUpperCase() + str.substr(1).toLowerCase()
+
 class BioStat {
   constructor(name, sex, age, height, weight) {
-    this.name = (typeof name === 'string' ? name.toLowerCase() : name)
+    this.name = titleCase(name)
     this.sex = sex
     this.age = Number(age)
     this.height = Number(height)
     this.weight = Number(weight)
+  }
+
+  isValidName() {
+    return typeof this.name === 'string' && this.name !== undefined
+  }
+
+  isValidSex() {
+    return typeof this.sex === 'string' && this.sex.length === 1 && 'MF'.includes(this.sex.toUpperCase())
+  }
+
+  isValidAge() {
+    return !Number.isNaN(this.age) && this.age >= 18
+  }
+
+  isValidHeight() {
+    return !Number.isNaN(this.height) && this.height > 0
+  }
+
+  isValidWeight() {
+    return typeof this.weight === 'number'
   }
 }
 
@@ -18,153 +40,131 @@ const readCSV = (path) => {
   const data = fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' })
   const optionsToObject = { delimiter: ',', quote: '"' }
   const objectContent = csvjson.toObject(data, optionsToObject)
-  return objectContent
+  const mapObject = new Map()
+  objectContent.forEach((object) => {
+    mapObject.set(object.name, object)
+  })
+  return mapObject
 }
 
-const writeCSV = (path, objectContent) => {
+const writeCSV = (path, statContent) => {
+  const arrayContent = [...statContent.values()]
   const optionsToCSV = { delimiter: ',', wrap: false, headers: 'key' }
-  const csvContent = csvjson.toCSV(objectContent, optionsToCSV)
+  const csvContent = csvjson.toCSV(arrayContent, optionsToCSV)
   fs.writeFileSync(path, csvContent)
   return true
 }
 
-const isNamePresent = (inputName, arr) => {
-  const profile = arr.find((person) => (inputName.toLowerCase() === (person.name).toLowerCase()))
-  if ((profile) !== undefined) {
-    return { first: true, second: profile }
+const validator = (inputData) => {
+  if (inputData.isValidName() === false) {
+    console.log('Name Invalid')
+    process.exit(1)
   }
-  return false
-}
 
-const checkerSex = (inputSex) => {
-  switch (inputSex) {
-    case 'm': case 'M':
-      return true
-    case 'f': case 'F':
-      return true
-    default:
-      console.log(`ERROR Invalid "${inputSex}" Sex Argument \nUSE ONLY: \n'M' : Male\n'F' : Female`)
-      process.exit(1)
+  if (inputData.isValidSex() === false) {
+    console.log('Sex Invalid')
+    process.exit(1)
   }
-  return undefined
-}
-
-const checkerAge = (inputAge) => {
-  if (Number.isNaN(inputAge) || inputAge < 18) {
-    console.log(`Invalid "${inputAge}" is invalid. Only ages 18 and above.`)
+  if (inputData.isValidAge() === false) {
+    console.log('Age Invalid')
+    process.exit(1)
+  }
+  if (inputData.isValidHeight() === false) {
+    console.log('Height Invalid')
+    process.exit(1)
+  }
+  if (inputData.isValidWeight() === false) {
+    console.log('Weight Invalid')
     process.exit(1)
   }
 }
 
-const checkIsNum = (inputNum) => {
-  if (Number.isNaN(inputNum) || inputNum < 0) {
-    console.log(`Invalid "${inputNum}" is invalid. Only number types.`)
+const statFinder = (existingStats, statName) => {
+  const foundStat = existingStats.get(statName)
+  if (!foundStat) {
+    console.log(`${statName} Does not exists`)
     process.exit(1)
   }
+  return foundStat
 }
 
-const createFlag = (inputArgs, objectContent, path) => {
-  const createElement = {}
-  if (argument.length < 6 || argument.length > 6) {
+const createStat = (existingStats, toCreateStat) => {
+  if (toCreateStat.length < 6 || toCreateStat.length > 6) {
     console.log('Invalid Number of arguments, All fields must be filled')
     process.exit(1)
-  } else if (!(isNamePresent(inputArgs.name, objectContent)).first === true) {
-    createElement.name = ((inputArgs.name).charAt(0).toUpperCase() + (inputArgs.name).slice(1))
-    if ((checkerSex(inputArgs.sex)) === true && (!(checkerAge(inputArgs.age)))
-    && (!checkIsNum(inputArgs.height)) && (!checkIsNum(inputArgs.weight))) {
-      createElement.sex = (inputArgs.sex).toUpperCase()
-      createElement.age = inputArgs.age
-      createElement.height = inputArgs.height
-      createElement.weight = inputArgs.weight
-    }
-    objectContent.push(createElement)
-    writeCSV(path, objectContent)
-    console.log(`Added Bio:\nName: ${createElement.name}\nSex: ${createElement.sex}\nAge: ${createElement.age}\nHeight: ${createElement.height} inchs\nWeight: ${createElement.weight} lbs`)
   } else {
-    console.log(`${inputArgs.name} already exists`)
-    process.exit(1)
+    const foundStat = existingStats.get(toCreateStat.name)
+    if (!foundStat) {
+      validator(toCreateStat)
+      existingStats.set(toCreateStat.name, toCreateStat)
+    }
   }
-  return objectContent
+  return existingStats
 }
 
-const readFlag = (inputArgs, objectContent) => {
-  if (argument.length < 2 || argument.length > 2) {
+const readStat = (existingStats, toFindStat) => {
+  if (toFindStat.length < 2 || toFindStat.length > 2) {
     console.log('Invalid Number of arguments')
     process.exit(1)
-  } else if (isNamePresent(inputArgs.name, objectContent).first === true) {
-    const foundPerson = (isNamePresent(inputArgs.name, objectContent)).second
-    const toFullSex = (_rSex) => (_rSex === 'M' ? 'Male' : 'Female')
-    console.log(`Name: ${foundPerson.name}\nSex: ${toFullSex(foundPerson.sex)}\nAge: ${foundPerson.age}\nHeight: ${foundPerson.height} inches, ${foundPerson.height * 2.54} cm\nWeight: ${foundPerson.weight} lbs, ${foundPerson.weight * 0.453592} kgs`)
-  } else {
-    console.log(`${inputArgs.name} not Found.`)
   }
-  return objectContent
+  return statFinder(existingStats, toFindStat.name)
 }
 
-const updateFlag = (inputArgs, objectContent, path) => {
-  const updateElement = {}
-  if (argument.length < 6 || argument.length > 6) {
+const updateStat = (existingStats, toUpdateStat) => {
+  if (toUpdateStat.length < 6 || toUpdateStat.length > 6) {
     console.log('Invalid Number of arguments, All fields must be filled')
     process.exit(1)
-  } else if ((isNamePresent(inputArgs.name, objectContent)).first === true) {
-    const foundPerson = (isNamePresent(inputArgs.name, objectContent)).second
-    updateElement.name = ((inputArgs.name).charAt(0).toUpperCase() + (inputArgs.name).slice(1))
-    if ((checkerSex(inputArgs.sex)) === true && (!(checkerAge(inputArgs.age)))
-    && (!checkIsNum(inputArgs.height)) && (!checkIsNum(inputArgs.weight))) {
-      updateElement.sex = (inputArgs.sex).toUpperCase()
-      updateElement.age = inputArgs.age
-      updateElement.height = inputArgs.height
-      updateElement.weight = inputArgs.weight
-    }
-    const updatedContent = objectContent.filter((person) => person !== foundPerson)
-    updatedContent.push(updateElement)
-    writeCSV(path, updatedContent)
-    console.log(`Updated biostats.csv file.\nAdded Bio:\nName: ${updateElement.name}\nSex: ${updateElement.sex}\nAge: ${updateElement.age}\nHeight: ${updateElement.height} inchs\nWeight: ${updateElement.weight} lbs`)
   } else {
-    console.log(`${inputArgs.name} not found`)
-    process.exit(1)
+    statFinder(existingStats, toUpdateStat.name)
+    validator(toUpdateStat)
+    existingStats.set(toUpdateStat.name, toUpdateStat)
   }
-  return objectContent
+  return existingStats
 }
 
-const deleteFlag = (inputArgs, objectContent, path) => {
-  if (argument.length < 2 || argument.length > 2) {
+const deleteStat = (existingStats, toDeleteStat) => {
+  if (toDeleteStat.length < 2 || toDeleteStat.length > 2) {
     console.log('Invalid Number of arguments')
     process.exit(1)
-  } else if ((isNamePresent(inputArgs.name, objectContent)).first === true) {
-    const foundPerson = (isNamePresent(inputArgs.name, objectContent)).second
-    const deletedContent = objectContent.filter((person) => person !== foundPerson)
-    console.log(`Successfuly Deleted Bio Stats of ${inputArgs.name}`)
-    writeCSV(path, deletedContent)
   } else {
-    console.log(`${inputArgs.name} not Found.`)
+    statFinder(existingStats, toDeleteStat.name)
+    existingStats.delete(toDeleteStat.name)
   }
-  return objectContent
+  return existingStats
 }
+
+const toFullSex = (_rSex) => (_rSex === 'M' ? 'Male' : 'Female')
 
 if (argument.length < 1) {
   console.log('Invalid lack of arguments')
 } else {
   const pathBiostats = 'biostats.csv'
-  const objectContent = readCSV(pathBiostats)
-  // console.log(objectContent)
+  const statsObject = readCSV(pathBiostats)
   switch (flag) {
-    case '-c':
-      console.log(`Creating ${name} ...`)
-      createFlag(new BioStat(name, sex, age, height, weight), objectContent, pathBiostats)
+    case '-c': {
+      const newBioSat = new BioStat(titleCase(name), String(sex).toUpperCase(), age, height, weight)
+      writeCSV(pathBiostats, createStat(statsObject, newBioSat))
+      console.log(`${BioStat.name} succesfully Created`)
       break
-    case '-r':
-      console.log(`Reading ${name} bio ...`)
-      readFlag(new BioStat(name), objectContent)
+    }
+    case '-r': {
+      const newBioSat = new BioStat(titleCase(name))
+      const foundStat = readStat(statsObject, newBioSat)
+      console.log(`Name: ${foundStat.name}\nSex: ${toFullSex(foundStat.sex)}\nAge: ${foundStat.age}\nHeight: ${foundStat.height} inches || ${(foundStat.height * 2.54).toFixed(2)} cm\nWeight: ${foundStat.weight}. lbs || ${(foundStat.weight * 0.453592).toFixed(2)} kgs`)
       break
-    case '-u':
-      console.log(`Updating ${name} bio ...`)
-      updateFlag(new BioStat(name, sex, age, height, weight), objectContent, pathBiostats)
+    }
+    case '-u': {
+      const newBioSat = new BioStat(titleCase(name), String(sex).toUpperCase(), age, height, weight)
+      writeCSV(pathBiostats, updateStat(statsObject, newBioSat))
+      console.log(`${BioStat.name} succesfully Updated`)
       break
-    case '-d':
-      console.log(`Deleting ${name} bio ...`)
-      deleteFlag(new BioStat(name), objectContent, pathBiostats)
+    }
+    case '-d': {
+      const newBioSat = new BioStat(titleCase(name))
+      writeCSV(pathBiostats, deleteStat(statsObject, newBioSat))
+      console.log(`${BioStat.name} succesfully Deleted`)
       break
+    }
     default:
       console.log(`ERROR Invalid "${flag}" Flag Argument \nUSE ONLY: \n'-c' : To Create/Add Bio\n'-r' : To Read/View Bio\n'-u' : To Update a Bio\n'-d' : To Delete a Bio`)
       process.exit(1)
